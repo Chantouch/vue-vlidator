1b077f000578ede5d2aa793c89551563b28985f5
Touch <s.chantouch@mango-byte.com>
2020-06-19 09:40:43 +0700

    add installer

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,19 @@
 import Validator from './validator';
 
-export const install = (Vue, options = {}) => {
-  Vue.mixin();
+const install = (Vue, options = {}) => {
+  Vue.mixin({
+    beforeCreate() {
+      this.$options.vlidator = {};
+      this.$options.$vlidator = {};
+      Vue.util.defineReactive(this.$options, '$vlidator', Validator);
+      if (!this.$options.computed) {
+        this.$options.computed = {};
+      }
+      this.$options.computed.$vlidator = function() {
+        return this.$options.$vlidator;
+      };
+    }
+  });
 };
 
 export default { install, Validator };
