2c36ea4fd0e93625e229f249dc621fcad4936f90
Touch <s.chantouch@mango-byte.com>
2020-06-24 09:20:08 +0700

    fixe version

--- nuxt/vue-vlidator-plugin.template.js
+++ nuxt/vue-vlidator-plugin.template.js
@@ -5,7 +5,7 @@ export default ({ app }) => {
     // inject options from module
     const i18n = app.i18n;
     const [pluginOptions] = [<%= serialize(options) %>]
-    if (i18n && i18n.locale) {
+    if (i18n && i18n.locale && !pluginOptions.locale) {
         const lang = require('@/lang/' + i18n.locale)
         const { attributes = {}, messages = {} } = lang.default ? lang.default : lang
         Object.assign(pluginOptions, {
